d8f151e3bcd0fad71ab98c6579939918a7698b01
MelikaSepidband <97177956+MelikaSepidband@users.noreply.github.com>
2025-07-29 10:39:31 -0500

    Update combine.py

--- rgfl/fl/combine.py
+++ rgfl/fl/combine.py
@@ -5,13 +5,13 @@ from collections import Counter
 
 from tqdm import tqdm
 
-from rgfl.util.utils import load_jsonl
-
+from agentless.util.utils import load_jsonl
 
 def combine_file_level(args):
-
     embed_used_locs = load_jsonl(args.retrieval_loc_file)
     model_used_locs = load_jsonl(args.model_loc_file)
+
+    embed_used_map = {x["instance_id"]: x["found_files"] for x in embed_used_locs}
 
     with open(f"{args.output_folder}/embed_used_locs.jsonl", "w") as f:
         for loc in embed_used_locs:
@@ -23,20 +23,19 @@ def combine_file_level(args):
 
     for pred in tqdm(model_used_locs, colour="MAGENTA"):
         instance_id = pred["instance_id"]
-
         model_loc = pred["found_files"]
-        retrieve_loc = [x for x in embed_used_locs if x["instance_id"] == instance_id][
-            0
-        ]["found_files"]
 
         combined_loc_counter = Counter()
-        combined_locs = []
 
+        # always include model locations
         for loc in model_loc[: args.top_n]:
             combined_loc_counter[loc] += 1
 
-        for loc in retrieve_loc[: args.top_n]:
-            combined_loc_counter[loc] += 1
+        # if present, include retrieval locations
+        if instance_id in embed_used_map:
+            retrieve_loc = embed_used_map[instance_id]
+            for loc in retrieve_loc[: args.top_n]:
+                combined_loc_counter[loc] += 1
 
         combined_locs = [loc for loc, _ in combined_loc_counter.most_common()]
 
@@ -53,7 +52,6 @@ def combine_file_level(args):
                 + "\n"
             )
 
-
 def main():
     parser = argparse.ArgumentParser()
 
@@ -61,7 +59,6 @@ def main():
     parser.add_argument("--output_file", type=str, default="combined_locs.jsonl")
     parser.add_argument("--retrieval_loc_file", type=str, required=True)
     parser.add_argument("--model_loc_file", type=str, required=True)
-    # supports file level (step-1) combination
     parser.add_argument("--top_n", type=int, required=True)
 
     args = parser.parse_args()
@@ -71,12 +68,10 @@ def main():
 
     os.makedirs(args.output_folder, exist_ok=True)
 
-    # dump argument
     with open(os.path.join(args.output_folder, "args.json"), "w") as f:
         json.dump(vars(args), f, indent=4)
 
     combine_file_level(args)
-
 
 if __name__ == "__main__":
     main()
